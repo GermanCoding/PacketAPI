4f1b03f70edf03a16aa5291b26040f43f3c54c5f
GermanCoding <Nummer378@aknm-craft.com>
2015-07-23 19:13:37 +0200

    Update license

--- PacketAPI/src/com/germancoding/packetapi/defaultpackets/KeepAlivePacket.java
+++ PacketAPI/src/com/germancoding/packetapi/defaultpackets/KeepAlivePacket.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Nummer378
+ * Copyright (c) 2015 Maximilian Fröhling alias Nummer378/GermanCoding
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
