001b9ac86ea8c59e4290a668f1f66039038cdeaa
GermanCoding <Nummer378@aknm-craft.com>
2015-07-23 19:17:18 +0200

    Fix jenkins failure

--- PacketAPI/src/com/germancoding/packetapi/udp/UnreliableSocket.java
+++ PacketAPI/src/com/germancoding/packetapi/udp/UnreliableSocket.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Maximilian Fröhling alias Nummer378/GermanCoding
+ * Copyright (c) 2015 Maximilian Froehling alias Nummer378/GermanCoding
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
